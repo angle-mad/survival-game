740f25d3a1aa40bce6ba7aa4b50955a6954cc587
Angel Madrigal <angelrmadrigal2009@gmail.com>
2025-12-22 18:45:28 -0800

    Finished basic game(health, healthbar, deathscreen, auto-spawning mobs)

--- characters/Objects/gun.gd
+++ characters/Objects/gun.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
-
+#AI WRITTEN - Somethng messed up when saving my file last time and the orginal code from a tutorial video just decided to stop working so I asked AI to fix what was wrong and I'm not really sure what happened originally but this works.
 func _physics_process(_delta: float) -> void:
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
-		var target_enemy = enemies_in_range.front()
-		look_at(target_enemy.global_position)
+		for body in enemies_in_range:  # Add this line!
+			if body.collision_layer == 2:  # Only target mobs on Layer 2
+				$WeaponPivot.look_at(body.global_position)
+				if $Timer.is_stopped():
+					$Timer.start()
+				break
+	else:
+		$Timer.stop()
+#End of AI WRITTEN code
 
 func shoot():
 	const BULLET = preload("res://bullet.tscn")
@@ -16,4 +23,4 @@ func shoot():
 
 
 func _on_timer_timeout() -> void:
-	shoot()
+	shoot() 
--- characters/Objects/player.gd
+++ characters/Objects/player.gd
@@ -1,6 +1,22 @@
 extends CharacterBody2D
 
+signal health_depleted
+
+var health = 100.0
+
 func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right","move_up" ,"move_down")
-	velocity = direction * 600
+	velocity = direction * 450
 	move_and_slide()
+
+	const DAMAGE_RATE = 25.0
+	var overlapping_mobs = %Hurtbox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * _delta
+		%ProgressBar.value = health
+		%ProgressBar.max_value = 100
+		if health <= 0.0:
+			health_depleted.emit()
+		
+		
+		
